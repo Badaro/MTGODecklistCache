50421b73685bb680d5c68bf236fded78b0022064
Filipe Donghia Badaro <badaro@gmail.com>
2020-09-02 12:53:46 -0300

    Some recent leagues had been generated with an outdated version of MTGODecklistParser, fixed those

--- Validator/Validator/Program.cs
+++ Validator/Validator/Program.cs
@@ -56,6 +56,20 @@ namespace Validator
             {
                 dynamic tournament = JsonConvert.DeserializeObject(File.ReadAllText(tournamentFile));
 
+                // Date validation
+                foreach (var deck in tournament.Decks)
+                {
+                    if (deck.Date != null && tournament.Tournament.Date.ToString() == deck.Date.ToString())
+                    {
+                        // A couple tournaments actually break the pattern, so they need to be whitelisted
+                        if (Path.GetFileNameWithoutExtension(tournamentFile) == "standard-ptq-2016-05-31") continue;
+                        if (Path.GetFileNameWithoutExtension(tournamentFile) == "pauper-preliminary-2019-12-16") continue;
+
+                        Console.WriteLine($"Tournament {Path.GetFileNameWithoutExtension(tournamentFile)} is using outdated data model");
+                        break;
+                    }
+                }
+
                 // Deck validation
                 if (tournament.Decks.Count == 0)
                 {
