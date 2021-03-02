384603018c14c0e2f4255db24762950991da1711
Filipe Donghia Badaro <badaro@gmail.com>
2021-03-02 18:18:38 -0300

    ManaTraders updater code ported to new structure

--- Tools/MTGODecklistCache.Updater.App/Program.cs
+++ Tools/MTGODecklistCache.Updater.App/Program.cs
@@ -1,4 +1,5 @@
-﻿using MTGODecklistCache.Updater.Wizards;
+﻿using MTGODecklistCache.Updater.Model;
+using MTGODecklistCache.Updater.Wizards;
 using Newtonsoft.Json;
 using System;
 using System.Globalization;
@@ -10,44 +11,56 @@ namespace MTGODecklistCache.Updater.App
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length < 2)
             {
-                Console.WriteLine("Usage: MTGODecklistCache.Updater.App CACHE_FOLDER [START_DATE] [END_DATE]");
+                Console.WriteLine("Usage: MTGODecklistCache.Updater.App CACHE_FOLDER RAWDATA_FOLDER [START_DATE] [END_DATE]");
                 return;
             }
 
             string cacheFolder = new DirectoryInfo(args[0]).FullName;
+            string rawDataFolder = new DirectoryInfo(args[1]).FullName;
+
             DateTime startDate = DateTime.Now.AddDays(-7).ToUniversalTime().Date;
-            if (args.Length > 1)
-            {
-                startDate = DateTime.Parse(args[1], CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal).ToUniversalTime();
-            }
-            DateTime? endDate = null;
             if (args.Length > 2)
             {
-                endDate = DateTime.Parse(args[2], CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal).ToUniversalTime();
+                startDate = DateTime.Parse(args[2], CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal).ToUniversalTime();
+            }
+            DateTime? endDate = null;
+            if (args.Length > 3)
+            {
+                endDate = DateTime.Parse(args[3], CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal).ToUniversalTime();
             }
 
-            UpdateWizards(Path.Combine(cacheFolder, "magic.wizards.com"), startDate, endDate);
+            // Updates Wizards cache folder
+            UpdateFolder(Path.Combine(cacheFolder, "magic.wizards.com"),
+                () => MTGODecklistCache.Updater.Wizards.TournamentList.GetTournaments(startDate, endDate, null, 7),
+                t => MTGODecklistCache.Updater.Wizards.TournamentLoader.GetTournamentDetails(t),
+                t => $"{Path.GetFileName(t.Uri.LocalPath)}.json");
+
+            // Updates ManaTraders cache folder
+            UpdateFolder(Path.Combine(cacheFolder, "manatraders.com"),
+                () => MTGODecklistCache.Updater.ManaTraders.TournamentList.GetTournaments(Path.Combine(rawDataFolder, "ManaTraders")),
+                t => MTGODecklistCache.Updater.ManaTraders.TournamentLoader.GetTournamentDetails(t),
+                t => $"{Path.GetFileNameWithoutExtension((t as MTGODecklistCache.Updater.ManaTraders.ManaTradersTournament).File).Replace("_", "-").ToLowerInvariant()}-{t.Date.ToString("yyyy-MM-dd")}.json");
         }
 
-        static void UpdateWizards(string cacheFolder, DateTime startDate, DateTime? endDate)
+        static void UpdateFolder(string cacheFolder, Func<Tournament[]> tournamentList, Func<Tournament, CacheItem> tournamentLoader, Func<Tournament, string> fileNameLoader)
         {
             Console.WriteLine("Downloading tournament list");
-            foreach (var tournament in TournamentList.GetTournaments(startDate, endDate, null, 7))
+            foreach (var tournament in tournamentList())
             {
                 Console.WriteLine($"Downloading tournament {tournament.Uri}");
                 string targetFolder = Path.Combine(cacheFolder, tournament.Date.Year.ToString(), tournament.Date.Month.ToString("D2").ToString(), tournament.Date.Day.ToString("D2").ToString());
                 if (!Directory.Exists(targetFolder)) Directory.CreateDirectory(targetFolder);
 
-                string targetFile = Path.Combine(targetFolder, $"{Path.GetFileName(tournament.Uri.LocalPath)}.json");
+                string targetFile = Path.Combine(targetFolder, fileNameLoader(tournament));
                 if (File.Exists(targetFile))
                 {
                     Console.WriteLine($"Already downloaded, skipping");
                     continue;
                 }
 
-                var details = TournamentLoader.GetTournamentDetails(tournament);
+                var details = tournamentLoader(tournament);
                 string contents = JsonConvert.SerializeObject(details, Formatting.Indented);
 
                 File.WriteAllText(targetFile, contents);
---
+++ Tools/MTGODecklistCache.Updater.ManaTraders/TournamentList.cs
@@ -0,0 +1,32 @@
+﻿using MTGODecklistCache.Updater.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MTGODecklistCache.Updater.ManaTraders
+{
+    public static class TournamentList
+    {
+        public static Tournament[] GetTournaments(string rawDataFolder)
+        {
+            List<Tournament> tournaments = new List<Tournament>();
+            foreach (string tournamentFile in Directory.GetFiles(rawDataFolder, "*.json"))
+            {
+                dynamic json = JsonConvert.DeserializeObject(File.ReadAllText(tournamentFile));
+                DateTime date = json.Date;
+                tournaments.Add(new ManaTradersTournament()
+                {
+                    Name = json.Name,
+                    Date = date.ToUniversalTime(),
+                    Uri = json.Url,
+                    Csv = json.Csv,
+                    Swiss = json.Swiss,
+                    File = Path.GetFileName(tournamentFile)
+                });
+            }
+            return tournaments.ToArray();
+        }
+    }
+}
