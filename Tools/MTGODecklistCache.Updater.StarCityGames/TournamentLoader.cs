85a210303ff1da15e4ee32a2676ae93422a29b0d
Filipe Donghia Badaro <badaro@gmail.com>
2021-11-01 12:31:19 -0300

    Placeholder to start working on SCG scraper

---
+++ Tools/MTGODecklistCache.Updater.StarCityGames/TournamentLoader.cs
@@ -0,0 +1,21 @@
+﻿using HtmlAgilityPack;
+using MTGODecklistCache.Updater.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace MTGODecklistCache.Updater.StarCityGames
+{
+    public static class TournamentLoader
+    {
+        public static CacheItem GetTournamentDetails(Tournament tournament)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
