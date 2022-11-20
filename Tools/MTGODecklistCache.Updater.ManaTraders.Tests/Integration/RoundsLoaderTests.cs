7b0b51642159636a24dc6b687dba35bced24c662
Filipe Badaro <badaro@gmail.com>
2022-11-20 18:26:41 -0300

    Basic round loader test for ManaTraders

---
+++ Tools/MTGODecklistCache.Updater.ManaTraders.Tests/Integration/RoundsLoaderTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using MTGODecklistCache.Updater.ManaTraders;
+using MTGODecklistCache.Updater.Model;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTGODecklistCache.Updater.MagicGG.Tests
+{
+    public class RoundsLoaderTests
+    {
+        private Round[] _testData = null;
+
+        [OneTimeSetUp]
+        public void GetTestData()
+        {
+            _testData = TournamentLoader.GetTournamentDetails(new Tournament()
+            {
+                Uri = new Uri("https://www.manatraders.com/tournaments/30/"),
+                Date = new DateTime(2022, 08, 31, 00, 00, 00, DateTimeKind.Utc)
+            }).Rounds;
+        }
+
+        [Test]
+        public void RoundCountIsCorrect()
+        {
+            _testData.Length.Should().Be(8);
+        }
+
+        [Test]
+        public void RoundsHaveNumber()
+        {
+            foreach (var round in _testData) round.RoundNumber.Should().BeGreaterThan(0);
+        }
+
+        [Test]
+        public void RoundsHaveMatches()
+        {
+            foreach (var round in _testData) round.Matches.Length.Should().BeGreaterThan(0);
+        }
+
+        [Test]
+        public void RoundDataIsCorrect()
+        {
+            Round testRound = _testData.First();
+            testRound.RoundNumber.Should().Be(1);
+            testRound.Matches.First().Should().BeEquivalentTo(new RoundItem()
+            {
+                Player1 = "SuperCow12653",
+                Player2 = "Demrakh",
+                Result = "2-0-0"
+            });
+        }
+    }
+}
